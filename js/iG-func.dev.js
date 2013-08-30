488882cd92bcddb4b2e5dbab63c06502264c43b2
Carlos Celma <solrac@igitsoft.com>
2013-08-29 23:50:54 -0500

    s

--- js/iG-func.dev.js
+++ js/iG-func.dev.js
@@ -1,21 +1,22 @@
 var serverFile='http://192.168.1.69/carlos/APPS/mitierraoaxaca/Web/fnc/ajaxfnc2.php';
 $(function(){
-    cargarTlayudas();
-    listenerComentarios = setInterval(function(){comentarios();},500);
-    //Tlayudas Preparadas
-    $(document).hammer().on("tap",'.preparada',function(){
-        var p=$(this).parent().attr('tlayuda');
-        preparada(p,$(this));
-    });
-    //Tlayudas Listas
-    $(document).hammer().on("tap",'.lista',function(){
-        isReady=$(this).parent().children('.ready').size();
-        if(isReady>0){
+    document.addEventListener("deviceready",function(){
+        cargarTlayudas();
+        listenerComentarios = setInterval(function(){comentarios();},500);
+        //Tlayudas Preparadas
+        $(document).hammer().on("tap",'.preparada',function(){
             var p=$(this).parent().attr('tlayuda');
-            calentada(p,$(this));
-        }
-    });
-    alert();
+            preparada(p,$(this));
+        });
+        //Tlayudas Listas
+        $(document).hammer().on("tap",'.lista',function(){
+            isReady=$(this).parent().children('.ready').size();
+            if(isReady>0){
+                var p=$(this).parent().attr('tlayuda');
+                calentada(p,$(this));
+            }
+        });
+    },false);
 });
 
 function cargarTlayudas(){
@@ -35,8 +36,8 @@ function syncTlayudas(){
         for(i=0;i<tlay.length;i++){
             exists=false;
             $('#home ul li').each(function(e){
-            if($(this).attr('tlayuda')==tlay[i].tlaId)
-                exists=true;
+                if($(this).attr('tlayuda')==tlay[i].tlaId)
+                    exists=true;
             });
             if(!exists)
                 $('#home ul').append('<li tlayuda="'+tlay[i].tlaId+'"><button class="preparada">Listo</button>'+tlay[i].producto+'<button class="lista">Listo</button><span class="comments"></span></li>');
