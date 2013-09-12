c4193ccf05780920dc9fc8351dbbc1a4062940f8
Carlos Celma <solrac@igitsoft.com>
2013-09-12 18:58:05 -0500

    d

--- js/iG-func.dev.js
+++ js/iG-func.dev.js
@@ -42,6 +42,7 @@ function syncTlayudas(){
             });
             if(!exists)
                 $('#home ul').append('<li tlayuda="'+tlay[i].tlaId+'"><table style="width=100%;"><tr><td rowspan="2"><button class="preparada">Listo</button></td><td>'+tlay[i].producto+'</td><td>Cliente</td><td>Mesero</td><td rowspan="2"><button class="lista">Listo</button></td> </tr><tr><td colspan="3" class="comments"></td></tr></table></li>');
+			alert(tlay[i].tlaId;
         }
     });
 }
