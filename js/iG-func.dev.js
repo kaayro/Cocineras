f7ed546fb824edb5d641d755d4c03d256673a052
Carlos Celma <solrac@igitsoft.com>
2013-09-12 19:41:45 -0500

    c

--- js/iG-func.dev.js
+++ js/iG-func.dev.js
@@ -10,7 +10,7 @@ $(function(){
         });
         //Tlayudas Listas
         $(document).hammer().on("tap",'.lista',function(){
-            isReady=$(this).parents('li').children('.ready').length;
+            isReady=$(this).parents('li').find('.ready').length;
 			alert(isReady);
             if(isReady>0){
                 var p=$(this).parent().attr('tlayuda');
